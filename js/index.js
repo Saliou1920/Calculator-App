73b57516dae7a9b2f055135745ea8b2cb6d449ae
Saliou1920 <diopsaliou45@gmail.com>
2021-06-17 23:51:57 -0300

    adding function to do the calculation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const listener = document.querySelectorAll(".pad-item");
+const result = document.getElementById('.results');
+
+console.log(result);
 let array = [];
 
 for (let index = 0; index < listener.length; index++) {
@@ -18,7 +21,23 @@ function saliou() {
   for (let index = 0; index < array.length; index++) {
     console.log(array[index]);
     
-    
+    if (index + 1 < array.length && (index + 1)%2 != 0) {
+      switch (array[index + 1]) {
+        case '+':
+          array[index + 2] += array[index]
+          break;
+        case '-':
+          array[index + 2] -= array[index]
+          break;
+        case '*':
+          array[index + 2] *= array[index]
+          break;
+              
+        default:
+          break;
+      }
+    }
   }
+  results.innerHTML = array[array.length - 1];
   
 }
