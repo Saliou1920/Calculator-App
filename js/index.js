67131740a8d074a1e415a2b17dfd926b5f9d5e2f
Saliou1920 <diopsaliou45@gmail.com>
2021-06-20 00:11:10 -0300

    adding calculator() and a click effect to show which button is selected

--- js/index.js
+++ js/index.js
@@ -1,43 +1,56 @@
 const listener = document.querySelectorAll(".pad-item");
-const result = document.getElementById('.results');
+const result = document.getElementById("resultat");
 
-console.log(result);
+console.log(result.classList[1]);
 let array = [];
-
 for (let index = 0; index < listener.length; index++) {
-  
   listener[index].addEventListener("click", function () {
-    if(this.textContent != '=') {
-      array.push(this.textContent);
+    buttonAnimation(this.classList[2]);
+    let getValeur = this.textContent;
+    if(getValeur != '=') {
+      array.push(getValeur);
     } else {
-      saliou();
+      calculator();
+      array = [];
     }
   });
   
 }
 
 
-function saliou() {
+function calculator() {
+  let resultat = 0;
   for (let index = 0; index < array.length; index++) {
     console.log(array[index]);
     
     if (index + 1 < array.length && (index + 1)%2 != 0) {
       switch (array[index + 1]) {
         case '+':
-          array[index + 2] += array[index]
+          array[index + 2] = parseInt(array[index]) + parseInt(array[index + 2]);
           break;
         case '-':
-          array[index + 2] -= array[index]
+          array[index + 2] =  parseInt(array[index]) - parseInt(array[index + 2]);
           break;
-        case '*':
-          array[index + 2] *= array[index]
+        case 'x':
+          array[index + 2] = parseInt(array[index + 2]) * parseInt(array[index]);
           break;
-              
+        case '/':
+          array[index + 2] = parseInt(array[index + 2]) / parseInt(array[index]);
+          break;      
         default:
+          console.error('404');
           break;
       }
     }
   }
-  results.innerHTML = array[array.length - 1];
-  
+  result.innerHTML = array[array.length -1];
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+      activeButton.classList.remove("pressed");
+  }, 100);
+
 }
